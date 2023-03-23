86bda112aea6c82a3d57624edd2e9e6c405935cd
rkaminski <50remek50@gmail.com>
2023-03-23 13:51:16 +0100

    zmiana dla szukania klucza cezara

--- projekt1/cezar.py
+++ projekt1/cezar.py
@@ -4,7 +4,6 @@ import sys
 def cezar():
     with open('plain.txt', 'r') as file:
         jawny = file.read()
-        print(jawny)
     with open('key.txt', 'r') as file:
         klucz = int(file.read().split(" ")[0])
         print(klucz)
@@ -24,10 +23,8 @@ def cezar():
 def odszyfrujcezar():
     with open('crypto.txt', 'r') as file:
         krypto = file.read()
-        print(krypto)
     with open('key.txt', 'r') as file:
         klucz = int(file.read().split(" ")[0])
-        print(klucz)
         if klucz < 0 and klucz > 25:
             print('Wrong key')
             return
@@ -40,11 +37,35 @@ def odszyfrujcezar():
                     file.write(chr((ord(i) - klucz - 97) % 26 + 97))
             else:
                 file.write(i)
-    
+
+def znajdzkluczcezar():
+    with open('crypto.txt', 'r') as file:
+        krypto = file.read()
+    with open('plain.txt', 'r') as file:
+        jawny = file.read()
+    with open('key-found.txt', 'w') as file:
+        for klucz in range(26):
+            with open('decrypt.txt', 'w') as file2:
+                for i in krypto:
+                    if i.isalpha():
+                        if i.isupper():
+                            file2.write(chr((ord(i) - klucz - 65) % 26 + 65))
+                        else:
+                            file2.write(chr((ord(i) - klucz - 97) % 26 + 97))
+                    else:
+                        file2.write(i)
+            with open('decrypt.txt', 'r') as file3:
+                odszyfrowany = file3.read()
+                if odszyfrowany == jawny:
+                    file.write(str(klucz))
+                    print("klucz to: ", klucz)
+                    return
+    print("Nie znaleziono klucza")
+
+
 def kryptogramcezar():
     with open('crypto.txt', 'r') as file:
         krypto = file.read()
-        print(krypto)
     with open('decrypt.txt', 'w') as file:
         for i in range(26):
             for j in krypto:
@@ -57,41 +78,6 @@ def kryptogramcezar():
                     file.write(j)
             file.write('\n')
 
-def znajdzkluczcezar():
-
-    with open('extra.txt', 'r') as file:
-        extra = file.read()
-        print(extra)
-    with open('crypto.txt', 'r') as file:
-        krypto = file.read()
-        print(krypto)
-    with open('plain.txt', 'r') as file:
-        jawny = file.read()
-        print(jawny)
-    with open('key-found.txt', 'w') as file:
-        for i in krypto:
-            for j in extra:
-                if i.isupper() or i.islower():
-                    klucz = (ord(i) - ord(j)) % 26
-                    file.write(str(klucz))
-                    with open('decrypt.txt', 'w') as file:
-                        for i in krypto:
-                            if i.isalpha():
-                                if i.isupper():
-                                    file.write(chr((ord(i) - klucz - 65) % 26 + 65))
-                                else:
-                                    file.write(chr((ord(i) - klucz - 97) % 26 + 97))
-                            else:
-                                file.write(i)
-                    with open('decrypt.txt', 'r') as file:
-                        odszyfrowany = file.read()
-                        print(odszyfrowany)
-                        if odszyfrowany == jawny:
-                            print("klucz to: ", klucz)
-                        else:
-                            print("Nie znaleziono klucza")
-                            return
-                    return
 
 def gcd(a, b):
     while b != 0:
@@ -101,7 +87,6 @@ def gcd(a, b):
 def afiniczny():
     with open('plain.txt', 'r') as file:
         jawny = file.read()
-        print(jawny)
     with open('key.txt', 'r') as file:
         klucz = file.read().split(" ")
         klucz1 = int(klucz[0])
@@ -129,13 +114,11 @@ def odwrotnosc_modulo(a, m):
 def odszyfrujafiniczny():
     with open('crypto.txt', 'r') as file:
         krypto = file.read()
-        print(krypto)
     with open('key.txt', 'r') as file:
         klucz = file.read().split(" ")
         klucz1 = int(klucz[0])
         klucz2 = int(klucz[1])
-        print(klucz1, klucz2)
-        if klucz1 < 0 and klucz1 > 25 or klucz2 < 0:
+        if klucz1 < 0 and klucz1 > 25 or klucz2 < 0 or gcd(klucz1, 26) != 1:
             print('Wrong key')
             return
         klucz1_odwrocony = odwrotnosc_modulo(klucz1, 26)
@@ -155,13 +138,10 @@ def odszyfrujafiniczny():
 def znajdzkluczafiniczny():
     with open('plain.txt', 'r') as file:
         jawny = file.read()
-        print(jawny)
     with open('crypto.txt', 'r') as file:
         krypto = file.read()
-        print(krypto)
     with open('decrypt.txt', 'r') as file:
         odszyfrowany = file.read()
-        print(odszyfrowany)
     with open('key-found.txt', 'w') as file:
         for i in range(26):
             for j in range(26):
@@ -186,7 +166,6 @@ def znajdzkluczafiniczny():
 def kryptogramafiniczny():
     with open('crypto.txt', 'r') as file:
         krypto = file.read()
-        print(krypto)
     with open('decrypt.txt', 'w') as file:
         for i in range(26):
             for j in range(26):
